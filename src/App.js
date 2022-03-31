49e59bd45d1b787fe148571cb8bb4444027905a9
david mchale <dmchaledesigner@gmail.com>
2022-03-31 14:28:56 +1100

    1.fully responsive site

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+
+// global styles
+import './globalCss/App.css';
+
+
+// import Container Components from the index file inside the main container folder 
+import {Blog, Features, Footer, Header, Possibility, WhatGpt3} from './containers'
+
+// import Main Components from Components Folder
+import {Cta, Navbar, Brand} from './components'
+
+const App = () => {
+  return (
+   <div className="app">
+     <div className="gradient__bg">
+       <Navbar />
+       <Header />
+     </div>
+
+     <Brand />
+     <WhatGpt3 />
+     <Features />
+     <Possibility />
+     <Cta />
+     <Blog />
+     <Footer />
+
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+// we creat this index file to hold all the individual components so we can import them to wherever we need
+
+export {default as Article} from './article/Article';
+export {default as Brand} from './brand/Brand';
+export {default as Cta} from './cta/Cta';
+export { default as Feature} from './feature/Feature';
+export { default as Navbar} from './navbar/Navbar';
